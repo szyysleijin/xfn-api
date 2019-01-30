39f43f49b17f707b69d2261c4522dc068ca7ca44
LeiJin <szyys1688@126.com>
2019-01-30 11:53:09 +0800

    测试接口

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
 
 const port = 8090;
 const express = require('express');
-const category = require('./routes/admin/category.js')
+const category = require('./routes/admin/category.js');
 const app=express();
 
 const cors = require('cors');
 
 
 app.listen(port,()=>{
-	console.log('数据api子系统'+port+'.......')
+	console.log('数据api子系统'+port+'.......');
 })
 
 //  中间件 cors
 app.use(cors());
 
 // 挂载路由
-app.use('/admin/category',category)
+app.use('/admin/category',category);
 
